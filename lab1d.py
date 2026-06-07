af84d9ced5ea2a03b1f12ffbcc48c5f87b9e2dad
Bapi Mohammed <bmohammed11@myseneca.ca>
2026-06-07 03:24:33 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program will output a math expression to the screen.'''
+
+x = 10
+y = 2
+z = 5
+
+answer = x + y * z
+
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(answer))
